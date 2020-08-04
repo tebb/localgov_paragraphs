199cbef8ef86ab6934fbb707f57387369bdcc6af
Stephen Cox <stephen-cox@users.noreply.github.com>
2020-08-04 11:37:30 +0100

    Feature/campaigns 25 paragraphs types (#5)
    * Renamed image paragraph type. localgovdrupal/localgov_campaigns/issues/25
    
    * Renamed text paragraph type. localgovdrupal/localgov_campaigns/issues/25
    
    * Renamed contact paragraph type and fields. localgovdrupal/localgov_campaigns/issues/25
    
    * Renamed link paragraphe type. localgovdrupal/localgov_campaigns/issues/25
    
    * Renamed remaining paragraph fields. localgovdrupal/localgov_campaigns/issues/25
    
    * Renamed remaining paragraph fields. localgovdrupal/localgov_campaigns/issues/25
    
    * Use media for image fields
    
    * Fix ordering of image paragraph form

--- tests/src/Functional/ParagraphsAdministrationTest.php
+++ tests/src/Functional/ParagraphsAdministrationTest.php
@@ -15,7 +15,6 @@ class ParagraphsAdministrationTest extends ParagraphsTestBase {
    * @var array
    */
   public static $modules = [
-    'paragraphs',
     'localgov_paragraphs',
   ];
 
@@ -29,43 +28,43 @@ class ParagraphsAdministrationTest extends ParagraphsTestBase {
 
     // Check paragraph types installed.
     $this->drupalGet('/admin/structure/paragraphs_type');
-    $this->assertSession()->pageTextContains('advanced_links');
-    $this->assertSession()->pageTextContains('contact');
-    $this->assertSession()->pageTextContains('pagebuilder_image');
-    $this->assertSession()->pageTextContains('pagebuilder_text');
+    $this->assertSession()->pageTextContains('localgov_contact');
+    $this->assertSession()->pageTextContains('localgov_image');
+    $this->assertSession()->pageTextContains('localgov_link');
+    $this->assertSession()->pageTextContains('localgov_text');
 
     // Check advanced_links paragraph fields.
-    $this->drupalGet('/admin/structure/paragraphs_type/advanced_links/fields');
-    $this->assertSession()->pageTextContains('field_title');
-    $this->assertSession()->pageTextContains('field_url');
+    $this->drupalGet('/admin/structure/paragraphs_type/localgov_link/fields');
+    $this->assertSession()->pageTextContains('localgov_title');
+    $this->assertSession()->pageTextContains('localgov_url');
 
     // Check contact paragraph fields.
-    $this->drupalGet('/admin/structure/paragraphs_type/contact/fields');
-    $this->assertSession()->pageTextContains('field_para_contact_address');
-    $this->assertSession()->pageTextContains('field_para_contact_url');
-    $this->assertSession()->pageTextContains('field_para_contact_email');
-    $this->assertSession()->pageTextContains('field_para_contact_facebook');
-    $this->assertSession()->pageTextContains('field_para_contact_heading');
-    $this->assertSession()->pageTextContains('field_para_contact_instagram');
-    $this->assertSession()->pageTextContains('field_para_contact_location');
-    $this->assertSession()->pageTextContains('field_para_contact_minicom');
-    $this->assertSession()->pageTextContains('field_para_contact_mobile');
-    $this->assertSession()->pageTextContains('field_para_contact_office_hours');
-    $this->assertSession()->pageTextContains('field_para_contact_other_social');
-    $this->assertSession()->pageTextContains('field_para_contact_other_url');
-    $this->assertSession()->pageTextContains('field_para_contact_out_of_hours');
-    $this->assertSession()->pageTextContains('field_para_contact_phone');
-    $this->assertSession()->pageTextContains('field_para_contact_subheading');
-    $this->assertSession()->pageTextContains('field_para_contact_twitter');
+    $this->drupalGet('/admin/structure/paragraphs_type/localgov_contact/fields');
+    $this->assertSession()->pageTextContains('localgov_contact_address');
+    $this->assertSession()->pageTextContains('localgov_contact_url');
+    $this->assertSession()->pageTextContains('localgov_contact_email');
+    $this->assertSession()->pageTextContains('localgov_contact_facebook');
+    $this->assertSession()->pageTextContains('localgov_contact_heading');
+    $this->assertSession()->pageTextContains('localgov_contact_instagram');
+    $this->assertSession()->pageTextContains('localgov_contact_location');
+    $this->assertSession()->pageTextContains('localgov_contact_minicom');
+    $this->assertSession()->pageTextContains('localgov_contact_mobile');
+    $this->assertSession()->pageTextContains('localgov_contact_office_hours');
+    $this->assertSession()->pageTextContains('localgov_contact_other_social');
+    $this->assertSession()->pageTextContains('localgov_contact_other_url');
+    $this->assertSession()->pageTextContains('localgov_contact_out_of_hours');
+    $this->assertSession()->pageTextContains('localgov_contact_phone');
+    $this->assertSession()->pageTextContains('localgov_contact_subheading');
+    $this->assertSession()->pageTextContains('localgov_contact_twitter');
 
     // Check pagebuilder_image paragraph fields.
-    $this->drupalGet('/admin/structure/paragraphs_type/pagebuilder_image/fields');
-    $this->assertSession()->pageTextContains('field_caption');
-    $this->assertSession()->pageTextContains('field_image');
+    $this->drupalGet('/admin/structure/paragraphs_type/localgov_image/fields');
+    $this->assertSession()->pageTextContains('localgov_caption');
+    $this->assertSession()->pageTextContains('localgov_image');
 
     // Check pagebuilder_text paragraph fields.
-    $this->drupalGet('/admin/structure/paragraphs_type/pagebuilder_text/fields');
-    $this->assertSession()->pageTextContains('field_text');
+    $this->drupalGet('/admin/structure/paragraphs_type/localgov_text/fields');
+    $this->assertSession()->pageTextContains('localgov_text');
   }
 
   /**
@@ -80,12 +79,12 @@ class ParagraphsAdministrationTest extends ParagraphsTestBase {
       'administer nodes',
     ]);
 
-    // Add a new paragraph node with a pagebuilder_text paragraph field.
+    // Add a new paragraph node with a text paragraph field.
     $this->drupalGet('node/add/paragraphs');
-    $this->drupalPostForm(NULL, [], 'field_paragraphs_pagebuilder_text_add_more');
+    $this->drupalPostForm(NULL, [], 'field_paragraphs_localgov_text_add_more');
     $edit = [
       'title[0][value]' => 'Test',
-      'field_paragraphs[0][subform][field_text][0][value]' => 'Test paragraph text',
+      'field_paragraphs[0][subform][localgov_text][0][value]' => 'Test paragraph text',
     ];
     $this->drupalPostForm(NULL, $edit, 'Save');
     $this->assertSession()->pageTextContains('has been created');
